1b5efcb36d35cb94abc48c49ba3a875a240d24f6
Wesley <wesleyaraujoteixeira98@gmail.com>
2024-12-10 03:35:49 -0300

    enviando alterações

--- lanche_storage/src/app/dashboard/components/orders/page.tsx
+++ lanche_storage/src/app/dashboard/components/orders/page.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useContext } from 'react';
 import styles from './orders.module.scss';
 import { TbRefresh } from "react-icons/tb";
 import { server } from '@/services/globalApi';
-import { getCookie } from '@/lib/cookiesClient';
 import { toast } from 'sonner';
-import { useContext } from 'react';
 import { OrderContext } from '@/app/providers/order';
 import Modal from '../modal/Modal';
 
@@ -21,23 +19,22 @@ interface Orders {
 }
 
 const Orders = () => {
-
-
     const { isOpen, onRequestOpen } = useContext(OrderContext);
 
-    const [token, setToken] = useState<string | null>(null); // State para armazenar o token
-
-    useEffect(() => {
-      // Garante que o token seja obtido apenas no client-side
-      const storedToken = localStorage.getItem('token');
-      setToken(storedToken);
-    }, []);
-
+    const [token, setToken] = useState<string | null>(null);
     const [orders, setOrders] = useState<Orders[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
 
+    // Obtém o token do localStorage no client-side
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        setToken(storedToken);
+    }, []);
+
     // Função para carregar pedidos
     const loadOrders = useCallback(async () => {
+        if (!token) return; // Certifica-se de que o token está disponível antes de executar a função
+
         setLoading(true);
         try {
             const response = await server.get('order', {
@@ -53,66 +50,61 @@ const Orders = () => {
         } finally {
             setLoading(false);
         }
-    }, []);
+    }, [token]);
 
-    // Carregar pedidos ao montar o componente
+    // Carrega pedidos sempre que o token muda
     useEffect(() => {
-        loadOrders();
-    }, [loadOrders]);
-
-
-
+        if (token) {
+            loadOrders();
+        }
+    }, [token, loadOrders]);
 
     const handleOpen = (id: string) => {
         onRequestOpen(id);
-    }
+    };
 
     return (
         <>
-        <div className={styles.container}>
-            <section className={styles.containerHeader}>
-                <h1>Últimos Pedidos</h1>
-                <span>
-                    <TbRefresh
-                        size={24}
-                        color='#3fffa3'
-                        onClick={loadOrders}
-                        style={{ cursor: 'pointer' }}
-                    />
-                </span>
-            </section>
+            <div className={styles.container}>
+                <section className={styles.containerHeader}>
+                    <h1>Últimos Pedidos</h1>
+                    <span>
+                        <TbRefresh
+                            size={24}
+                            color='#3fffa3'
+                            onClick={loadOrders}
+                            style={{ cursor: 'pointer' }}
+                        />
+                    </span>
+                </section>
 
-            <section className={styles.listOrders}>
-                {loading ? (
-                    <p>Carregando pedidos...</p>  // Mostra uma mensagem de carregamento
-                ) : (
-                    <>
-                        {orders.length > 0 ? (
-                            orders.map((item) => (
-                                <button
-                                
+                <section className={styles.listOrders}>
+                    {loading ? (
+                        <p>Carregando pedidos...</p> // Mostra uma mensagem de carregamento
+                    ) : (
+                        <>
+                            {orders.length > 0 ? (
+                                orders.map((item) => (
+                                    <button
+                                        onClick={() => handleOpen(item.order_id)}
+                                        key={item.order_id}
+                                        className={styles.orderItem}
+                                    >
+                                        <div className={styles.tag}></div>
+                                        <span>Mesa {item.table}</span>
+                                    </button>
+                                ))
+                            ) : (
+                                <div className={styles.messageAlert}>
+                                    <h2>Nenhum pedido encontrado.</h2>
+                                </div> // Exibe mensagem caso não haja pedidos
+                            )}
+                        </>
+                    )}
+                </section>
+            </div>
 
-                                    onClick={ () => handleOpen(item.order_id)}
-                                    key={item.order_id}
-                                    className={styles.orderItem}
-                                >
-                                    <div className={styles.tag}></div>
-                                    <span>Mesa {item.table}</span>
-                                </button>
-                            ))
-                        ) : (
-                            <div className={styles.messageAlert} > 
-                                <h2>Nenhum pedido encontrado.</h2>
-                            </div>  // Exibe mensagem caso não haja pedidos
-                        )}
-                    </>
-                )}
-            </section>
-        </div>
-
-                {isOpen ? (
-                        <Modal/>
-                ) : null}
+            {isOpen && <Modal />}
         </>
     );
 };
